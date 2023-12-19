46239e92ec40c82e39cf5be8ad2531cf565ef913
Fajla Rabbee <thewebdevonline@gmail.com>
2023-12-19 23:45:29 +0600

    Added loader

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -2,11 +2,10 @@
 <!-- sidebar menu overlay -->
 <div x-cloak class="fixed inset-0 z-50 bg-[black]/60 lg:hidden" :class="{'hidden' : !$store.app.sidebar}" @click="$store.app.toggleSidebar()"></div>
 
-{{--<x-loader/>--}}
+<x-loader/>
 
 <x-scroll-to-top/>
 
-{{--@include('layouts.backend.customizer')--}}
 
 <div class="main-container min-h-screen text-black dark:text-white-dark" :class="[$store.app.navbar]">
     @include('layouts.backend.sidebar')
